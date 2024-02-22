e186d3e9520dd5e36d379625f164dd496c051e4c
Daksh Torul <45713920+urjkue@users.noreply.github.com>
2024-02-23 00:54:10 +0400

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,46 +1,54 @@
-import yfinance as yf
+import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 import matplotlib.pyplot as plt
+def main(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-known_stocks = ['AAPL', 'GOOGL', 'MSFT', 'TSLA', 'UBER', 'NFLX']  # List of known stocks
 
-stock_tickers = []
-while True:
-    print("Known stocks:", known_stocks)
-    ticker = input("Enter a stock ticker (or 'done' to finish): ")
-    if ticker.lower() == 'done':
-        print(stock_tickers)
-        break
-    stock_tickers.append(ticker.upper())
+    table = soup.find('table')
 
-start_date = pd.Timestamp.today().strftime('%Y-01-01')
-end_date = pd.Timestamp.today().strftime('%Y-%m-%d')
 
-df = pd.DataFrame()
-for ticker in stock_tickers:
-    data = yf.download(ticker, start=start_date, end=end_date)
-    print(data)
-    data = data[['Adj Close']]
-    data.columns = [ticker]
-    print(ticker)
-    df = pd.concat([df, data], axis=1)
+    df = pd.read_html(str(table))[0]
 
-df.index = pd.to_datetime(df.index)
+    output_file = "t.xlsx"
+    df.to_excel(output_file, index=False)
 
-# Plotting
-plt.figure(figsize=(12, 6))
-for column in df.columns:
-    plt.plot(df.index, df[column], label=column)
-plt.title('Stock Performance')
-plt.xlabel('Date')
-plt.ylabel('Adjusted Close Price')
-plt.legend()
-plt.grid(True)
-plt.xticks(rotation=45)
-plt.tight_layout()
+    print(f"Table data saved to {output_file}.")
 
-plt.show()
 
-output_file = 'user_stock_performance.xlsx'
-df.to_excel(output_file)
-print(f"Stock performance data saved to {output_file}.")
+
+    return 0
+
+def chart():
+    data = pd.read_excel('t.xlsx')
+    columns_of_interest = ['Last Closing Price', 'Change', '% Change']
+    columns_of_interest = columns_of_interest[:5]
+
+    companies = data['Company'][:5]
+    last_closing_prices = data['Last Closing Price'][:5]
+
+    fig, ax = plt.subplots()
+
+    for i, column in enumerate(columns_of_interest):
+        values = data[column][:5]
+        x = [i + 1 for i in range(len(values))]
+        ax.scatter(companies, values, label=column)
+    plt.xlabel('Company')
+    plt.ylabel('Last Closing Price')
+    plt.title('Stock Data')
+    for i, company in enumerate(companies):
+        ax.text(i + 1, last_closing_prices[i], company, ha='center', va='bottom')
+    plt.legend()
+
+    plt.show()
+
+
+
+
+
+
+main("https://www.stockexchangeofmauritius.com/products-market-data/equities-board/trading-quotes/official")
+chart()
+
